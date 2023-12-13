a2bfa1b7eb1109447da5d73e6413ad00fba177b0
TEuler27 <edoardo.tolotti@rinonline.com>
2023-12-13 17:33:57 +0100

    Added day 13

---
+++ 13/solve2.jl
@@ -0,0 +1,51 @@
+using IterTools
+
+function is_sym(i, mapp, row = true)
+	if !row
+		mapp = permutedims(mapp)
+	end
+	rows, _ = size(mapp)
+	found_smudge = false
+	for k in 0: min(i, rows - i)-1
+		# print("Checking row ", i-k, " and ", i+1+k, '\n')
+		if mapp[i - k, :] != mapp[i + k + 1, :]
+			if length(filter(x -> x == 0, mapp[i - k, :] .== mapp[i + k + 1, :])) > 1
+				return false
+			else
+				found_smudge = true
+			end
+		end
+	end
+	return found_smudge
+end
+
+function main()
+	sol = 0
+	lines = readlines("input")
+	split_indices = findall(x -> length(x) == 0, lines)
+	push!(split_indices, length(lines)+1)
+	pushfirst!(split_indices, 0)
+	for (start, endd) in IterTools.partition(split_indices, 2, 1)
+		mapp = reduce(vcat, permutedims.(collect.(lines[start+1:endd-1])))
+		(rows, columns) = size(mapp)
+		found = false
+		for i in 1:rows-1
+			if is_sym(i, mapp)
+				sol += 100 * i
+				found = true
+				break
+			end
+		end
+		if !found
+			for i in 1:columns-1
+				if is_sym(i, mapp, false)
+					sol += i
+					break
+				end
+			end
+		end
+	end
+	print("Soluzione: ", sol)
+end
+
+main()
